46c1dd27b5f954b1e84de8c230ac5fdd44b0ceba
Ace Alrich <hex0@live.com>
2021-01-11 22:13:37 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Canvas } from "react-three-fiber";
+import { OrbitControls, Stars } from "drei";
+import { Physics, usePlane, useBox } from "use-cannon";
+import "./styles.css";
+
+function Box(props) {
+  const [ref, api] = useBox(() => ({ mass: 1, position: [0, 2, 0] }));
+  return (
+    <mesh
+      onClick={() => {
+        api.velocity.set(0, 2, 0);
+      }}
+      ref={ref}
+      position={[0, 2, 0]}
+    >
+      <boxBufferGeometry attach="geometry" />
+      <meshLambertMaterial attach="material" color="hotpink" />
+    </mesh>
+  );
+}
+
+function Plane(props) {
+  const [ref] = usePlane(() => ({
+    rotation: [-Math.PI / 2, 0, 0],
+  }));
+  return (
+    <mesh rotation={[-Math.PI / 2, 0, 0]}>
+      <planeBufferGeometry attach="geometry" args={[100, 100]} />
+      <meshLambertMaterial attach="material" color="lightblue" />
+    </mesh>
+  );
+}
+
+export default function App() {
+  return (
+    <Canvas>
+      <OrbitControls />
+      <Stars />
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 15, 10]} angle={0.3} />
+      <Physics>
+        <Box />
+        <Plane />
+      </Physics>
+    </Canvas>
+  );
+}
